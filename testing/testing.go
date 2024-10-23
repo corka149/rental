17393bd1d5bc43c1af259211705f1b86cdb84d63
corka149 <corka149@mailbox.org>
2024-10-23 22:04:48 +0200

    Log calls to geten

--- testing/testing.go
+++ testing/testing.go
@@ -4,6 +4,7 @@ package rentaltesting
 import (
 	"cmp"
 	"context"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
@@ -22,7 +23,7 @@ func Setup() *cmd.Server {
 	gin.SetMode(gin.TestMode)
 	ctx := context.TODO()
 
-	s, err := cmd.NewServer(ctx, getenv)
+	s, err := cmd.NewServer(ctx, log(getenv))
 
 	if err != nil {
 		panic(err)
@@ -57,6 +58,14 @@ func Teardown(queries *datastore.Queries) {
 
 	for _, object := range objects {
 		queries.DeleteObject(context.TODO(), object.ID)
+	}
+}
+
+func log(getenv func(string) string) func(string) string {
+	return func(key string) string {
+		value := getenv(key)
+		fmt.Println(key, value)
+		return value
 	}
 }
 
