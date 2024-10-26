bb08eb11005e3cff21e56d86b19bf8c9fca6adde
corka149 <corka149@mailbox.org>
2024-10-26 20:06:27 +0200

    Add test

--- app/holidays_test.go
+++ app/holidays_test.go
@@ -8,8 +8,11 @@ import (
 	"net/url"
 	"strings"
 	"testing"
+	"time"
 
+	"github.com/corka149/rental/datastore"
 	rentaltesting "github.com/corka149/rental/testing"
+	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -112,6 +115,52 @@ func TestCreateHoliday(t *testing.T) {
 	assert.Equal(t, "Test Holiday", holidays[0].Title)
 	assert.Equal(t, beginning, holidays[0].Beginning.Time.Format("2006-01-02"))
 	assert.Equal(t, ending, holidays[0].Ending.Time.Format("2006-01-02"))
+
+	// Teardown
+	rentaltesting.Teardown(s.Queries)
+}
+
+func TestCreateHolidayFailsWhenItOverlaps(t *testing.T) {
+	// Arrange
+	s := rentaltesting.Setup()
+	w := httptest.NewRecorder()
+
+	password := "password"
+	user := rentaltesting.CreateUser(s.Queries, password)
+
+	beginning := "2024-10-20"
+	ending := "2024-10-30"
+
+	existing := rentaltesting.CreateHoliday(s.Queries, func(params *datastore.CreateHolidayParams) {
+		params.Beginning = pgtype.Date{Time: time.Date(2024, 10, 25, 0, 0, 0, 0, time.UTC), Valid: true}
+		params.Ending = pgtype.Date{Time: time.Date(2024, 10, 27, 0, 0, 0, 0, time.UTC), Valid: true}
+	})
+
+	formData := url.Values{}
+	formData.Set("beginning", beginning)
+	formData.Set("ending", ending)
+	formData.Set("title", "Test Holiday")
+
+	// Act
+	req, err := http.NewRequest("POST", "/holidays/new", strings.NewReader(formData.Encode()))
+
+	assert.Nil(t, err)
+
+	rentaltesting.Login(s, req, user.Email, password)
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	s.Router.ServeHTTP(w, req)
+
+	// Assert
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Contains(t, w.Body.String(), "There is a conflict with a holiday.")
+
+	holidays, err := s.Queries.GetHolidays(context.Background())
+
+	assert.Nil(t, err)
+	assert.Len(t, holidays, 1)
+	assert.Equal(t, existing.ID, holidays[0].ID)
 
 	// Teardown
 	rentaltesting.Teardown(s.Queries)
